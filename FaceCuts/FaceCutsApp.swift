c62a2205e84b0686ef9142c610e36c6bd5159dc1
tomokisun <github@tomokisun.com>
2024-11-22 21:35:54 +0900

    refactor: talk to founder

--- FaceCuts/FaceCutsApp.swift
+++ FaceCuts/FaceCutsApp.swift
@@ -1,16 +1,27 @@
 import SwiftUI
 import StoreKit
 
+private func openInstagram() {
+  let url = URL(string: "https://instagram.com/tomokisun")!
+  if UIApplication.shared.canOpenURL(url) {
+    UIApplication.shared.open(url)
+  }
+}
+
 class SceneDelegate: UIResponder, UIWindowSceneDelegate{
   func windowScene(_ windowScene: UIWindowScene, performActionFor shortcutItem: UIApplicationShortcutItem, completionHandler: @escaping (Bool) -> Void) {
-    let url = URL(string: "https://instagram.com/tomokisun")!
-    UIApplication.shared.open(url)
+    if shortcutItem.type == "talk-to-founder" {
+      openInstagram()
+    }
     completionHandler(true)
   }
 }
 
 class AppDelegate: NSObject, UIApplicationDelegate {
   func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+    if options.shortcutItem?.type == "talk-to-founder" {
+      openInstagram()
+    }
     let config = UISceneConfiguration(name: nil, sessionRole: connectingSceneSession.role)
     config.delegateClass = SceneDelegate.self
     return config
